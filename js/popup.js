958fd4700faa312adeb638a13b94cdaf32485535
Kimyeonjin1230 <kyj9750322@naver.com>
2024-05-09 18:38:52 +0900

    gitignore

--- js/popup.js
+++
@@ -1,5 +0,0 @@
- // 돋보기 아이콘 클릭 이벤트 핸들러
-    document.getElementById("search-icon").addEventListener("click", function() {
-        // 레이어 팝업 표시
-        document.getElementById("popup").style.display = "block";
-    });
